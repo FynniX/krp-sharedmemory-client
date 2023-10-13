1e2d2e0012248eaabf0c6ddad9dabbfcef259cdd
Fynn Haupt <47634930+FynniX@users.noreply.github.com>
2023-10-14 00:57:49 +0200

    added m_LastSplit, m_LastSpeed, m_LastCommunication, m_LastVehicleData to classification entry

--- lib/types/SPluginsRaceClassificationEntry.ts
+++ lib/types/SPluginsRaceClassificationEntry.ts
@@ -1,10 +1,18 @@
+import { SPluginsRaceCommunication } from "./SPluginsRaceCommunication";
 import { SPluginsRaceLap } from "./SPluginsRaceLap";
+import { SPluginsRaceSpeed } from "./SPluginsRaceSpeed";
+import { SPluginsRaceSplit } from "./SPluginsRaceSplit";
+import { SPluginsRaceVehicleData } from "./SPluginsRaceVehicleData";
 
 export type SPluginsRaceClassificationEntry = {
   m_iRaceNum: number;
   m_iState: number;
   m_iBestLap: number;
   m_LastLap: SPluginsRaceLap;
+  m_LastSplit: SPluginsRaceSplit;
+  m_LastSpeed: SPluginsRaceSpeed;
+  m_LastCommunication: SPluginsRaceCommunication;
+  m_LastVehicleData: SPluginsRaceVehicleData;
   m_fBestSpeed: number;
   m_iBestLapNum: number;
   m_iNumLaps: number;
--- src/RaceClassificationInfoWorker.cpp
+++ src/RaceClassificationInfoWorker.cpp
@@ -57,13 +57,58 @@ void RaceClassificationInfoWorker::OnOK()
         Napi::Array m_aiSplit = Napi::Array::New(Env(), GetArrayLength(data.m_RaceEntries[i1].m_LastLap.m_aiSplit));
         for (int i = 0; i < GetArrayLength(data.m_RaceEntries[i1].m_LastLap.m_aiSplit); i++)
             m_aiSplit[i] = data.m_RaceEntries[i1].m_LastLap.m_aiSplit[i];
-            
+
         m_LastLap.Set("m_aiSplit", m_aiSplit);
         m_LastLap.Set("m_fSpeed", data.m_RaceEntries[i1].m_LastLap.m_fSpeed);
         m_LastLap.Set("m_iBest", data.m_RaceEntries[i1].m_LastLap.m_iBest);
         m_LastLap.Freeze();
-
         obj.Set("m_LastLap", m_LastLap);
+
+        Napi::Object m_LastSplit = Napi::Object::New(Env());
+        m_LastSplit.Set("m_iSession", data.m_RaceEntries[i1].m_LastSplit.m_iSession);
+        m_LastSplit.Set("m_iSessionSeries", data.m_RaceEntries[i1].m_LastSplit.m_iSessionSeries);
+        m_LastSplit.Set("m_iRaceNum", data.m_RaceEntries[i1].m_LastSplit.m_iRaceNum);
+        m_LastSplit.Set("m_iLapNum", data.m_RaceEntries[i1].m_LastSplit.m_iLapNum);
+        m_LastSplit.Set("m_iSplit", data.m_RaceEntries[i1].m_LastSplit.m_iSplit);
+        m_LastSplit.Set("m_iSplitTime", data.m_RaceEntries[i1].m_LastSplit.m_iSplitTime);
+        m_LastSplit.Freeze();
+        obj.Set("m_LastSplit", m_LastSplit);
+
+        Napi::Object m_LastSpeed = Napi::Object::New(Env());
+        m_LastSpeed.Set("m_iSession", data.m_RaceEntries[i1].m_LastSpeed.m_iSession);
+        m_LastSpeed.Set("m_iSessionSeries", data.m_RaceEntries[i1].m_LastSpeed.m_iSessionSeries);
+        m_LastSpeed.Set("m_iRaceNum", data.m_RaceEntries[i1].m_LastSpeed.m_iRaceNum);
+        m_LastSpeed.Set("m_iLapNum", data.m_RaceEntries[i1].m_LastSpeed.m_iLapNum);
+        m_LastSpeed.Set("m_fSpeed", data.m_RaceEntries[i1].m_LastSpeed.m_fSpeed);
+        m_LastSpeed.Freeze();
+        obj.Set("m_LastSpeed", m_LastSpeed);
+
+        Napi::Object m_LastCommunication = Napi::Object::New(Env());
+        m_LastCommunication.Set("m_iSession", data.m_RaceEntries[i1].m_LastCommunication.m_iSession);
+        m_LastCommunication.Set("m_iSessionSeries", data.m_RaceEntries[i1].m_LastCommunication.m_iSessionSeries);
+        m_LastCommunication.Set("m_iRaceNum", data.m_RaceEntries[i1].m_LastCommunication.m_iRaceNum);
+        m_LastCommunication.Set("m_iCommunication", data.m_RaceEntries[i1].m_LastCommunication.m_iCommunication);
+        m_LastCommunication.Set("m_iState", data.m_RaceEntries[i1].m_LastCommunication.m_iState);
+        m_LastCommunication.Set("m_iReason", data.m_RaceEntries[i1].m_LastCommunication.m_iReason);
+        m_LastCommunication.Set("m_iOffence", data.m_RaceEntries[i1].m_LastCommunication.m_iOffence);
+        m_LastCommunication.Set("m_iLap", data.m_RaceEntries[i1].m_LastCommunication.m_iLap);
+        m_LastCommunication.Set("m_iType", data.m_RaceEntries[i1].m_LastCommunication.m_iType);
+        m_LastCommunication.Set("m_iTime", data.m_RaceEntries[i1].m_LastCommunication.m_iTime);
+        m_LastCommunication.Freeze();
+        obj.Set("m_LastCommunication", m_LastCommunication);
+
+        Napi::Object m_LastVehicleData = Napi::Object::New(Env());
+        m_LastVehicleData.Set("m_iRaceNum", data.m_RaceEntries[i1].m_LastVehicleData.m_iRaceNum);
+        m_LastVehicleData.Set("m_iActive", data.m_RaceEntries[i1].m_LastVehicleData.m_iActive);
+        m_LastVehicleData.Set("m_iRPM", data.m_RaceEntries[i1].m_LastVehicleData.m_iRPM);
+        m_LastVehicleData.Set("m_iGear", data.m_RaceEntries[i1].m_LastVehicleData.m_iGear);
+        m_LastVehicleData.Set("m_fSpeedometer", data.m_RaceEntries[i1].m_LastVehicleData.m_fSpeedometer);
+        m_LastVehicleData.Set("m_fSteer", data.m_RaceEntries[i1].m_LastVehicleData.m_fSteer);
+        m_LastVehicleData.Set("m_fThrottle", data.m_RaceEntries[i1].m_LastVehicleData.m_fThrottle);
+        m_LastVehicleData.Set("m_fBrake", data.m_RaceEntries[i1].m_LastVehicleData.m_fBrake);
+        m_LastVehicleData.Freeze();
+        obj.Set("m_LastVehicleData", m_LastVehicleData);
+
         obj.Set("m_fBestSpeed", data.m_RaceEntries[i1].m_fBestSpeed);
         obj.Set("m_iBestLapNum", data.m_RaceEntries[i1].m_iBestLapNum);
         obj.Set("m_iNumLaps", data.m_RaceEntries[i1].m_iNumLaps);
